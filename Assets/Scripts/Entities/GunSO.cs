0fcef5a58a5590421159b5738cc7d4fb49ae5a8d
wkembel1563 <wkembel@hotmail.com>
2022-08-26 10:58:52 -0500

    improved fire rate

--- Assets/Scripts/Entities/GunSO.cs
+++ Assets/Scripts/Entities/GunSO.cs
@@ -10,28 +10,28 @@ namespace PC.Entities
         [SerializeField] public float Range = 100f;
         [SerializeField] public float Damage = 20f;
         [SerializeField] public int MagazineSize = 30;
-        [SerializeField] private float _fireRate = 500f; // Rounds per minute
-        private float h_fireRate = float.MinValue;
-        public float FireRate
-        {
-            get
-            {
-                Debug.Log(_fireRate);
-                Debug.Log(60f / _fireRate);
-                if (h_fireRate == float.MinValue)
-                    h_fireRate = RatePerMinToDelay(_fireRate);
-                Debug.Log(h_fireRate);
-                return h_fireRate;
-            }
-        }
+        [SerializeField] public float FireRate = 10f; // Rounds per second
+        //private float h_fireRate = float.MinValue;
+        //public float FireRate
+        //{
+        //    get
+        //    {
+        //        Debug.Log(_fireRate);
+        //        Debug.Log(60f / _fireRate);
+        //        if (h_fireRate == float.MinValue)
+        //            h_fireRate = RatePerMinToDelay(_fireRate);
+        //        Debug.Log(h_fireRate);
+        //        return h_fireRate;
+        //    }
+        //}
         [SerializeField] public float ReloadTime = 3.0f;
         [SerializeField] public float VerticalRecoil = 2f;
         [SerializeField] public float HorizontalRecoil = 0.5f;
 
-        private float RatePerMinToDelay(float ratePerMin)
-        {
-            Debug.Log($"Inside: {60f / ratePerMin}");
-            return 60f / ratePerMin;
-        }
+        //private float RatePerMinToDelay(float ratePerMin)
+        //{
+        //    Debug.Log($"Inside: {60f / ratePerMin}");
+        //    return 60f / ratePerMin;
+        //}
     }
 }
