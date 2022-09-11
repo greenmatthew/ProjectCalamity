32fc6b7e175537bea1ccd2f179554e52a37b149d
wkembel1563 <wkembel@hotmail.com>
2022-09-11 12:27:40 -0500

    main camera set. initial cam direction needs fixing

---
+++ Assets/Scripts/Entities/CameraChange.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+
+namespace PC.Entities
+{
+    public class CameraChange : MonoBehaviour
+    {
+        #region Fields
+
+        #region Consts Fields
+        #endregion Consts Fields
+
+        #region Public Fields
+        #endregion Public Fields
+
+        #region Protected Fields
+        #endregion Protected Fields
+
+        #region Private Fields
+        #endregion Private Fields
+        [SerializeField] private GameObject _mainCam = null;
+        [SerializeField] private GameObject _rifleCam = null;
+
+        #endregion Fields
+
+    //----------------------------------------------------------------------------------------------------------------------
+
+        #region Methods
+    
+        #region Public Methods
+        #endregion Public Methods
+
+        #region Protected Methods
+        #endregion Protected Methods
+
+        #region Private Methods
+
+        private void Awake()
+        {
+            // make sure cameras exist
+            if(!_mainCam || !_rifleCam)
+            {
+                Debug.LogError("Player cameras not assigned. See Camera Monitor."); 
+            }
+            else
+            {
+                // only use main camera for now
+                // rifle cam will be used for ADS view and needs to be swappable for diff weapons
+                _rifleCam.SetActive(false);
+                _mainCam.SetActive(true);
+            }
+        }
+
+        private void Update()
+        {
+        
+        }
+    
+        #endregion Private Methods
+
+        #endregion Methods
+
+    //----------------------------------------------------------------------------------------------------------------------
+
+        #region Enums, Structs, Classes
+        #endregion Enums, Structs, Classes
+    }
+}
