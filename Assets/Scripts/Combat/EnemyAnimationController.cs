3cf5412489517000b7751f0ea7d3c1eb4111f722
wkembel1563 <wkembel@hotmail.com>
2022-12-02 01:43:28 -0600

    enemy walking animations complete

--- Assets/Scripts/Combat/EnemyAnimationController.cs
+++ Assets/Scripts/Combat/EnemyAnimationController.cs
@@ -17,8 +17,9 @@ namespace PC.Combat
 
         #region Private Fields
         [SerializeField] private Animator _animator;
-        [SerializeField] private Rigidbody _enemyRB;
         [SerializeField] private UnityEngine.AI.NavMeshAgent _navMeshAgent;
+        [SerializeField] private Transform _enemyTransform = null;
+        private Vector3 lastFacing = new Vector3(0, 0, 0);
         #endregion Private Fields
 
         #endregion Fields
@@ -45,20 +46,27 @@ namespace PC.Combat
                 _animator.SetBool("IsAttacking", false);
             }
 
+            lastFacing = _enemyTransform.forward;
+
         }
 
         private void Update()
         {
-            // track the enemy's velocity and angular velocity
-            //Debug.Log("Enemy's velocity: " + _enemyRB.velocity.magnitude);
-            //Debug.Log("Enemy's angular velocity: " + _enemyRB.angularVelocity.x);
-            Debug.Log("Enemy's velocity: " + _navMeshAgent.velocity.magnitude);
-            Debug.Log("Enemy's angular velocity: " + _enemyRB.angularVelocity.y * 100);
+            // calculate angular velocity from forward vector
+            Vector3 forward = _enemyTransform.forward;
+            //Vector3 angularVelocity = Vector3.Cross(forward, _enemyRB.velocity);
+            //float angularVelocity = Vector3.SignedAngle(_enemyTransform.forward, _navMeshAgent.velocity, Vector3.up);
+            float angularVelocity = Vector3.Angle(forward, lastFacing) / Time.deltaTime;
+            lastFacing = forward;
+
+            Debug.Log("LinearVelocity:" +  _navMeshAgent.velocity.magnitude);
+            Debug.Log("AngularVelocity:" + angularVelocity);
+
             // set angular velocity of rigid body
             if (_animator)
             {
-                _animator.SetFloat("LinearVelocity", _enemyRB.velocity.magnitude);
-                _animator.SetFloat("AngularVelocity", _enemyRB.angularVelocity.magnitude);
+                _animator.SetFloat("LinearVelocity", _navMeshAgent.velocity.magnitude);
+                _animator.SetFloat("AngularVelocity", angularVelocity);
             }
 
 
